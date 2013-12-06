0d4339ef0576346df571e93e8aa33224bfc335b1
Henry Stigler <stiglerh@gmail.com>
2013-12-05 20:03:19 -0500

    Added RESTful API to Clubs controller, tested json with Postman

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,15 +1,21 @@
 class ClubsController < ApplicationController
   before_action :set_club, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery with: :null_session #fixed "Can't verify CSRF token authenticity error, however not sure if this opens up security hole"
+  respond_to :html, :json
+
 
   # GET /clubs
   # GET /clubs.json
   def index
     @clubs = Club.all
+    respond_with @clubs, :include => :id
   end
 
   # GET /clubs/1
   # GET /clubs/1.json
   def show
+    @club = Club.find(params[:id])
+    respond_with @club, :include => :id
   end
 
   # GET /clubs/new
@@ -19,46 +25,31 @@ class ClubsController < ApplicationController
 
   # GET /clubs/1/edit
   def edit
+    @club = Club.find(params[:id])
   end
 
   # POST /clubs
   # POST /clubs.json
   def create
     @club = Club.new(club_params)
-
-    respond_to do |format|
-      if @club.save
-        format.html { redirect_to @club, notice: 'Club was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @club }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
-      end
-    end
+    @club.save
+    respond_with @club
   end
 
   # PATCH/PUT /clubs/1
   # PATCH/PUT /clubs/1.json
   def update
-    respond_to do |format|
-      if @club.update(club_params)
-        format.html { redirect_to @club, notice: 'Club was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
-      end
-    end
+   @club = Club.find(params[:id])
+   @club.update_attributes(club_params)
+   respond_with @club
   end
 
   # DELETE /clubs/1
   # DELETE /clubs/1.json
   def destroy
+    @club = Club.find(params[:id])
     @club.destroy
-    respond_to do |format|
-      format.html { redirect_to clubs_url }
-      format.json { head :no_content }
-    end
+    respond_with @club
   end
 
   private
@@ -69,6 +60,6 @@ class ClubsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def club_params
-      params.require(:club).permit(:identifier, :make, :model, :order, :name)
+      params.require(:club).permit(:identifier, :player_id, :make, :model, :order, :name)
     end
 end
--- app/views/clubs/show.json.jbuilder
+++ app/views/clubs/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @club, :identifier, :player_id, :make, :model, :order, :name, :created_at, :updated_at
+json.extract! @club, :id, :identifier, :player_id, :make, :model, :order, :name, :created_at, :updated_at
